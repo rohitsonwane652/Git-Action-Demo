c20f6ff338b45f5848a0c2c390053b8b84cee08d
rohitsonwane652 <rohitsonwane652@gmail.com>
2025-02-11 15:34:36 +0530

    added api

--- src/main/java/com/actions/gitactions/controller/HelloController.java
+++ src/main/java/com/actions/gitactions/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello() {
         return "Hello World!";
     }
+    
+    @GetMapping(value = "/hello")
+    public String helloWorld() {
+        return "Hello";
+    }
 }
